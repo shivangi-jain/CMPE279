605db92743a5302b32c9ef9b3e492507fcbae589
Shivangi Jain <55163106+shivangi-jain@users.noreply.github.com>
2020-03-26 01:20:14 -0500

    Port 8080 to port 80

--- assignment1/server.c
+++ assignment1/server.c
@@ -28,7 +28,7 @@ int main(int argc, char const *argv[])
         exit(EXIT_FAILURE); 
     } 
 
-    // Forcefully attaching socket to the port 8080 
+    // Forcefully attaching socket to the port 80
     if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, 
                                                   &opt, sizeof(opt))) 
     { 
@@ -39,7 +39,7 @@ int main(int argc, char const *argv[])
     address.sin_addr.s_addr = INADDR_ANY; 
     address.sin_port = htons( PORT ); 
        
-    // Forcefully attaching socket to the port 8080 
+    // Forcefully attaching socket to the port 80
     if (bind(server_fd, (struct sockaddr *)&address,  
                                  sizeof(address))<0) 
     { 
