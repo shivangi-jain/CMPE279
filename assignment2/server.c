1dbf3fe49ea50751fd6412bc179f19b8524c6091
Shivangi Jain <shivangi.jain@sjsu.edu>
2020-04-13 00:22:40 -0700

    Rename server file( changed in prev commit by mistake)

--- assignment2/server.c
+++ assignment2/server.c
@@ -17,10 +17,23 @@ int main(int argc, char const *argv[])
     int opt = 1; 
     int addrlen = sizeof(address); 
     char buffer[1024] = {0}; 
-    char *hello = "Hello from server"; 
+    char *hello = "Hello from server";
     char fd_arg[3]; 
-	
-    
+     
+    printf("argv[0] is %d and argc %d \n", argv[0][1], argv[0][2]);
+
+    //Here argc[0][2] is initialised to 12. I am using this value to make  
+    //sure that only child executes this code. Any value could have been   
+    //used in place of 12 but I have passed 12 in exec, I am comparing with 	//12.
+    if(argv[0][2] == 12)
+    {
+        int fd = argv[0][1];
+        valread = read( fd , buffer, 1024); 
+        printf("%s\n",buffer ); 
+        send(fd, hello , strlen(hello) , 0 );
+	printf("Hello message sent\n");
+	return 0; 
+    }    
     // Creating socket file descriptor 
     if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) 
     { 
@@ -56,6 +69,7 @@ int main(int argc, char const *argv[])
     { 
         perror("accept"); 
         exit(EXIT_FAILURE); 
+	
     } 
 
     //dropping privilege part
@@ -63,21 +77,32 @@ int main(int argc, char const *argv[])
     //Child 
     if(n == 0)
       {
+	struct passwd *privuser;
+	privuser = getpwnam("nobody");
+	if(privuser == NULL)
+	{
+	    exit(EXIT_FAILURE);
+	}
+	int userid = setuid(privuser->pw_uid);
+	printf("%d", userid);
+	
+	//fd_arg is to pass the socket descripter and also to pass a reference 		//value 12 , so that it can be compared and the code designated for 		//child can be executed.
         fd_arg[0] = new_socket;
         fd_arg[1] = new_socket;
         fd_arg[2] = 12;
 	printf("process id is %d", n);
 	printf("Value of argv is %s", argv[0]);
 	printf("Value of socket and argv %d %d", new_socket, fd_arg[0]);
-	execl(argv[0], fd_arg, NULL); 
-         
+	execl(argv[0], fd_arg, NULL);
       }
+
+	
     //Parent
-    else
+    else if(n > 0)
       {
+	printf("process id  parent is %d", n);
 	//Wait for the child process to finish
 	wait(NULL);
       }
     return 0; 
 } 
-
